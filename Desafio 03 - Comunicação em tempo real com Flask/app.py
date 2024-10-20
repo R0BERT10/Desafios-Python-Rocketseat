a701b69dca89c35610a517379c601b06b84bc421
Roberto Luna <estoquefripal@gmail.com>
2024-10-20 11:28:10 -0300

    feat: :building_construction: Added index.html rendering and added websocket io

--- Desafio 03 - Comunicação em tempo real com Flask/app.py
+++ Desafio 03 - Comunicação em tempo real com Flask/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+    socketio.emit('message', data)
+
+@socketio.on('connect')
+def connect(auth):
+    print("Conectado", auth)
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
